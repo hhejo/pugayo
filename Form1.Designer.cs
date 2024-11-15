467825bd4ba3ec2548445a6140d30f84f5fc74bc
hhejo <litsmainjof@gmail.com>
2024-11-15 16:20:36 +0900

    feat: update main

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PuGaYo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.labelQuery = new System.Windows.Forms.Label();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox.Location = new System.Drawing.Point(13, 13);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(458, 33);
+            this.TextBox.TabIndex = 0;
+            this.TextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+            // 
+            // listView
+            // 
+            this.listView.Activation = System.Windows.Forms.ItemActivation.TwoClick;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(13, 89);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(775, 436);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.ItemActivate += new System.EventHandler(this.listView_ItemActivate);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "제목";
+            this.columnHeader1.Width = 700;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "번호";
+            // 
+            // labelQuery
+            // 
+            this.labelQuery.AutoSize = true;
+            this.labelQuery.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuery.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelQuery.Location = new System.Drawing.Point(12, 53);
+            this.labelQuery.Name = "labelQuery";
+            this.labelQuery.Size = new System.Drawing.Size(190, 25);
+            this.labelQuery.TabIndex = 2;
+            this.labelQuery.Text = "검색어를 입력하세요";
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnStart.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStart.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.BtnStart.Location = new System.Drawing.Point(477, 12);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(103, 34);
+            this.BtnStart.TabIndex = 3;
+            this.BtnStart.Text = "검색 실행";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(590, 49);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(198, 34);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 4;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(586, 19);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(169, 21);
+            this.labelStatus.TabIndex = 5;
+            this.labelStatus.Text = "안녕하세요, 함희주님!";
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSave.Enabled = false;
+            this.BtnSave.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSave.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.BtnSave.Location = new System.Drawing.Point(477, 49);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(103, 34);
+            this.BtnSave.TabIndex = 6;
+            this.BtnSave.Text = "CSV 저장";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 537);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.labelQuery);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.TextBox);
+            this.Name = "Form1";
+            this.Text = "구글 검색기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Label labelQuery;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button BtnSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PuppeteerSharp;
+
+namespace PuGaYo
+{
+    public partial class Form1 : Form
+    {
+        private bool isSearching = false; // 현재 구글 검색중인지
+        private List<Dictionary<string, string>> searchResults; // 검색 결과 딕셔너리의 리스트
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 검색창 엔터 입력
+        private async void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // 엔터키를 누르지 않았거나, 현재 구글 검색중인 경우
+            if (e.KeyCode != Keys.Enter || isSearching)
+            {
+                return;
+            }
+            // 검색어를 입력하지 않은 경우
+            string query = TextBox.Text; // 검색어
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return;
+            }
+            // 구글 검색 시작 전 값 초기화
+            labelQuery.Text = TextBox.Text; // 입력한 검색어 출력
+            TextBox.Text = ""; // 검색창 초기화
+            listView.Items.Clear(); // 리스트뷰 아이템 초기화
+            SetLabelStatus(""); // 상태창 초기화
+            SetProgressBarValue(0); // 프로그레스바 초기화
+            SetBtnSaveState(false); // 저장 버튼 비활성화
+            try
+            {
+                SetLabelStatus("초기 설정중...");
+                SetIsSearching(true); // 검색중
+                IBrowser browser = await InitBrowser(); // Browser 초기화
+                IPage page = await InitPage(browser, query); // Page 생성, 구글 검색 URL 연결
+                searchResults = await PerformGoogleSearchAsync(page); // 구글 검색 실행하고 결과 데이터 획득
+                SetIsSearching(false); // 검색 완료
+                SetLabelStatus("데이터 검색 성공!");
+                await Task.Delay(2000);
+                int maxCount = searchResults.Count; // 검색 결과 수
+                SetProgressBarMaximum(maxCount - 1); // 프로그레스바 최댓값 설정
+                foreach (var (searchResult, index) in searchResults.Select((value, index) => (value, index)))
+                {
+                    AddListViewItem(searchResult, index); // searchResult를 리스트뷰에 아이템으로 추가
+                    SetProgressBarValue(index); // 프로그레스바 표시
+                    SetLabelStatus($"{index} / {maxCount}"); // 상태창 진행률 표시
+                    await Task.Delay(30); // 0.03초 대기
+                }
+                SetBtnSaveState(true); // 저장 가능
+                SetLabelStatus($"{maxCount}개 검색 완료");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void SetIsSearching(bool isSearched) => isSearching = isSearched;
+
+        private void SetLabelStatus(string statusMessage) => labelStatus.Text = statusMessage;
+
+        private void SetProgressBarValue(int value) => progressBar.Value = value;
+
+        private void SetProgressBarMaximum(int maximum) => progressBar.Maximum = maximum;
+
+        private void SetBtnSaveState(bool isEnabled) => BtnSave.Enabled = isEnabled;
+
+        private void AddListViewItem(Dictionary<string, string> searchResult, int currentCount)
+        {
+            ListViewItem listViewItem = new ListViewItem(searchResult["title"]); // 제목
+            listViewItem.SubItems.Add($"{currentCount + 1}"); // 번호
+            listViewItem.SubItems.Add(searchResult["link"]); // 링크
+            listView.Items.Add(listViewItem); // 리스트 뷰에 아이템 추가
+        }
+
+        // 구글 검색 실행
+        private async Task<List<Dictionary<string, string>>> PerformGoogleSearchAsync(IPage page)
+        {
+            List<Dictionary<string, string>> results = new List<Dictionary<string, string>>(); // 검색 결과들의 리스트
+            var (currentPage, hasNextPage) = (1, true); // 현재 페이지, 다음 페이지 보유 여부
+            while (hasNextPage)
+            {
+                SetLabelStatus(labelStatus.Text.Length > 12 ? "데이터 검색중" : labelStatus.Text + '.');
+                string expression = @"Array.from(document.querySelectorAll('div h3')).map(h3 => h3.innerText);";
+                var texts = await page.EvaluateExpressionAsync<List<string>>(expression); // h3 태그 내용 획득
+                expression = @"Array.from(document.querySelectorAll('a h3')).map(h3 => h3.parentElement.href);";
+                var hrefs = await page.EvaluateExpressionAsync<List<string>>(expression); // a 태그 href 획득
+                for (int i = 0; i < texts.Count && i < hrefs.Count; i++)
+                {
+                    var item = new Dictionary<string, string> {{ "title", texts[i] }, { "link", hrefs[i] }};
+                    results.Add(item);
+                }
+                expression = @"document.getElementById('pnnext') !== null";
+                hasNextPage = await page.EvaluateExpressionAsync<bool>(expression); // 다음 페이지 탐색
+                if (!hasNextPage)
+                {
+                    break; // 다음 페이지가 없으면 종료
+                }
+                await page.ClickAsync("#pnnext"); // 다음 페이지 이동 클릭
+                await page.WaitForNavigationAsync(); // 이동한 페이지 대기
+                await Task.Delay(new Random().Next(200, 300));
+                currentPage++;
+            }
+            return results;
+        }
+
+        // Browser 초기화
+        private async Task<IBrowser> InitBrowser()
+        {
+            var browserFetcher = new BrowserFetcher();
+            await browserFetcher.DownloadAsync();
+            var launchOptions = new LaunchOptions
+            {
+                Headless = true,
+                ExecutablePath = "C:/Program Files/Google/Chrome/Application/chrome.exe"
+            };
+            IBrowser browser = await Puppeteer.LaunchAsync(launchOptions);
+            return browser;
+        }
+
+        // 구글 검색 준비
+        private async Task<IPage> InitPage(IBrowser browser, string query)
+        {
+            var page = await browser.NewPageAsync(); // Page 생성
+            string searchUrl = $"https://www.google.com/search?q={Uri.EscapeDataString(query)}"; // 구글 검색 URL
+            await page.GoToAsync(searchUrl); // 해당 URL로 이동
+            return page;
+        }
+
+        private void listView_ItemActivate(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count <= 0)
+            {
+                return;
+            }
+            var selectedItem = listView.SelectedItems[0]; // 클릭한 아이템
+            string link = selectedItem.SubItems[2].Text; // 해당 아이템의 링크
+            try
+            {
+                System.Diagnostics.Process.Start("chrome.exe", link); // 해당 링크로 크롬 실행
+                selectedItem.BackColor = Color.SkyBlue;
+                selectedItem.ForeColor = Color.White;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"크롬 실행 오류: {ex}");
+            }
+        }
+
+        // 결과를 CSV 파일로 저장
+        private async Task SaveResultsToCSV(List<Dictionary<string, string>> searchResults, string filepath)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filepath, false, Encoding.UTF8))
+                {
+                    writer.WriteLine("번호,제목,링크");
+                    int index = 1;
+                    foreach (var searchResult in searchResults)
+                    {
+                        string title = searchResult.ContainsKey("title") ? searchResult["title"] : "";
+                        string link = searchResult.ContainsKey("link") ? searchResult["link"] : "";
+                        writer.WriteLine($"\"{index}\",\"{title}\",\"{link}\"");
+                        index++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"파일 저장 오류: {ex.Message}");
+            }
+        }
+
+        // 저장 버튼 클릭
+        private async void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("CSV 파일로 저장할까요?", "CSV 파일 저장", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                return;
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv",
+                Title = "Save results to CSV file"
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filepath = saveFileDialog.FileName;
+                await SaveResultsToCSV(searchResults, filepath);
+                MessageBox.Show("CSV 파일 저장 성공");
+            }
+        }
+    }
+}
